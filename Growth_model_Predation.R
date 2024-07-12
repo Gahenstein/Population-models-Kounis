fe391199aa1d5e9897de41a6c6ea8e06692f0c99
Gahenstein <emil.kounis@gmail.com>
2024-07-12 16:29:47 +0100

    Update Growth_model_Predation.R

--- Growth_model_Predation.R
+++ Growth_model_Predation.R
@@ -1,7 +1,7 @@
 # Population growth model with predation
 # 08/07/2024 AIMILIOS PASCHALIS KOUNIS
 ### Dependencies----
-source("Growth_Model_K/Model_Files/Growth_Model_K.R")
+source("Growth_Model_K.R")
 ### Argument explanation----
 # a : vector of 6 numbers with initial cohort pop for prey
 # b : vector of 6 numbers with initial cohort pop for pred
